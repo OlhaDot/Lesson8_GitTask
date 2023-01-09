b4eabd11c7640a147578d69b881596db752b577c
OlhaDot <dotsenko.raif@gmail.com>
2023-01-09 23:54:29 +0200

    fifth commit -1st commit in Test - test_func creation

--- mane.py
+++ mane.py
@@ -1,13 +1,5 @@
-def mane_function(a, b):
-    if a == 0:
-        print("A = 0")
-    elif a == b:
-        print("A = B")
-    elif a > b:
-        print("A > B")
-    else:
-        print("other situation")
+def test_function():
+    print("New function was added")
 
-mane_function(3, 5)
 
 
